66d30b8877676bddcffb18d3bc105dd0967630fd
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-10-19 15:04:18 -0300

    test(demo): add demo layout

--- src/test/java/com/flowingcode/vaadin/addons/simpletimer/DemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/simpletimer/DemoView.java
@@ -19,78 +19,22 @@
  */
 package com.flowingcode.vaadin.addons.simpletimer;
 
-
-import java.math.BigDecimal;
-
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
 
 /**
  * @author Leonardo Scardanzan / Flowing Code
  */
-@Route("")
 @SuppressWarnings("serial")
-public class DemoView extends Div {
+@Route("")
+public class DemoView extends VerticalLayout implements BeforeEnterObserver {
 
-	public DemoView() {
-		this.setSizeFull();
-        final SimpleTimer timer = new SimpleTimer();
-        timer.setWidth("100px");
-        timer.setHeight("50px");
-        timer.getStyle().set("font-size", "40px");
-        
-        Span timerTitle = new Span("Simple Count Up Timer");
-        
-        final TextField startTime = new TextField("Start Time", e -> timer.setStartTime(new BigDecimal(e.getValue())));
-        final Checkbox countUp = new Checkbox("Count Up", false);
-		countUp.addValueChangeListener(e -> {
-			timer.setCountUp(countUp.getValue());
-			if (e.getValue()) {
-				startTime.setLabel("End Time");
-				timerTitle.setText("Simple Count Up Timer");
-			} else {
-				startTime.setLabel("Start Time");
-				timerTitle.setText("Simple Countdown Timer");
-			}
-		});
-        final Button start = new Button("Start/Stop", e -> timer.start());
-        final Button stop = new Button("Stop", e -> timer.pause());
-        final Button reset = new Button("Reset", e -> {
-            timer.reset();
-        });
-		final Button running = new Button("Current Time", e -> timer.getCurrentTimeAsync().thenAccept(
-				time -> Notification.show(time.toPlainString() + (timer.isRunning() ? "" : " (Not Running)"))));
-        final Checkbox fractions = new Checkbox("Fractions", true);
-        fractions.addValueChangeListener(e -> timer.setFractions(e.getValue()));
-        final Checkbox minutes = new Checkbox("Minutes", e -> timer.setMinutes(e.getValue()));
-        final Checkbox hours = new Checkbox("Hours", e -> timer.setHours(e.getValue()));
-        final Checkbox visible = new Checkbox("Visible", e->{
-        	if (e.isFromClient()) timer.setVisible(!timer.isVisible());	
-        });
-        visible.setValue(true);
-
-        timer.addTimerEndEvent(e -> Notification.show("Timer Ended"));
-
-		final HorizontalLayout topLayout = new HorizontalLayout(timerTitle, timer);
-        topLayout.setAlignItems(Alignment.CENTER);
-
-		HorizontalLayout options = new HorizontalLayout(countUp, fractions, minutes, hours, visible);
-		options.setAlignItems(Alignment.CENTER);
-
-		final HorizontalLayout bottomLayout = new HorizontalLayout(start, stop, reset, running);
-		bottomLayout.setAlignItems(Alignment.BASELINE);
-
-		add(new VerticalLayout(topLayout, startTime, options, bottomLayout));
-
+	@Override
+	public void beforeEnter(BeforeEnterEvent event) {
+		event.forwardTo(SimpletimerDemoView.class);
 	}
 
-
 }
+
