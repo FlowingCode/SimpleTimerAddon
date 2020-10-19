2b9c820ef4abf20a01abd5cdb3598d91e75041f9
Nicolas Gonzalez Paz <ngonzalez@flowingcode.com>
2020-10-19 15:41:13 -0300

    Added license header

--- src/test/java/com/flowingcode/vaadin/addons/DemoLayout.java
+++ src/test/java/com/flowingcode/vaadin/addons/DemoLayout.java
@@ -1,8 +1,8 @@
 /*-
  * #%L
- * TwinColGrid add-on
+ * Simple Timer Addon
  * %%
- * Copyright (C) 2017 - 2020 Flowing Code
+ * Copyright (C) 2019 - 2020 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
