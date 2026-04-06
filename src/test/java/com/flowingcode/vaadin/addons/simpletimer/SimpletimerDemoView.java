0e67baf7787f6f3c725732a72a1d2a9141c3c284
Paola De Bartolo <paola.debartolo@gmail.com>
2026-04-06 17:05:26 -0300

    chore: update license headers

--- src/test/java/com/flowingcode/vaadin/addons/simpletimer/SimpletimerDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/simpletimer/SimpletimerDemoView.java
@@ -2,7 +2,7 @@
  * #%L
  * Simple Timer Addon
  * %%
- * Copyright (C) 2019 - 2020 Flowing Code
+ * Copyright (C) 2019 - 2026 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
